a4baf489d8b52bf7bccbb87bc4cd4d4033ab7201
Fabiola Villanueva <22103326@usc.edu.ph>
2025-02-24 15:57:43 +0800

    add: R CODE FINALLY CONFIGURED LEFT ME CRYING

---
+++ R Files/installPackages.R
@@ -0,0 +1,12 @@
+# Set CRAN mirror
+chooseCRANmirror(ind = 1)  # Use the first mirror in the list
+
+# Install packages
+install.packages("AdhereR")
+install.packages("dplyr")
+install.packages("plyr")
+install.packages("lubridate")
+install.packages("latticeExtra")
+install.packages("data.table")
+install.packages("factoextra")
+install.packages("ggplot2")
